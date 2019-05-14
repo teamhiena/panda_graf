4aa2b7867a0988a2267deddca000e63f5ee48a9b
Miklós Beregszászi <beregmiklos@sch.bme.hu>
2019-05-14 18:53:25 +0200

    kommentek

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
-import java.util.HashMap;
-import java.util.Scanner;
-
 /**
- * A prototipushoz tartozo Main osztaly, mely tartalmazza a main() fuggvenyt.
+ * A grafikus valtozathoz tartozo Main osztaly, mely tartalmazza a main() fuggvenyt.
  */
 public class Main {
 	public static void main(String[] args) {
 
-        //TODO legyen singleton amit tud
         View view = new View();
         Game game = new Game();
         GameMap gameMap = new GameMap();
